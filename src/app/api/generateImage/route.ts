fd9b47d472290d9bd3231524b312a49b64de8890
Obah Sylva <sylvan452@gmail.com>
2024-01-02 14:40:17 +0100

    updated

--- src/app/api/generateImage/route.ts
+++ src/app/api/generateImage/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(request: Request) {
     const res = await request.json();
     const prompt = res.prompt;
