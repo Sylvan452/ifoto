9e169aa0c1071bb9e3dbb78cda82c377a5f30205
Obah Sylva <sylvan452@gmail.com>
2024-01-02 14:15:46 +0100

    force dynamic update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Images from '../../components/Images';
 import './globals.css';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <main className="mx-0">
